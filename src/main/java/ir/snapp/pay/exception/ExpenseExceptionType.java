b1ed15b65427475105508e379c919289d8a1851a
ali kamali <mohammad.jalalkamali@dfreight.org>
2024-02-06 12:05:27 +0330

    feat: add multi language into error message exception

--- src/main/java/ir/snapp/pay/exception/ExpenseExceptionType.java
+++ src/main/java/ir/snapp/pay/exception/ExpenseExceptionType.java
@@ -5,21 +5,17 @@ import ir.snapp.pay.util.MessageTranslatorUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.context.i18n.LocaleContextHolder;
-
-import java.util.Locale;
 
 @Getter
 @ToString
 @AllArgsConstructor
 public enum ExpenseExceptionType {
-	DEFAULT_EXCEPTION(1, "Exception In Expense API");
+	DEFAULT_EXCEPTION(1, "exception.in.expense.api");
 
 	private int errorCode;
 	private String errorMessage;
 
 	public String getErrorMessage() {
-		Locale locale = LocaleContextHolder.getLocale();
-		return MessageTranslatorUtil.getText(errorMessage, locale);
+		return MessageTranslatorUtil.getText(errorMessage);
 	}
 }

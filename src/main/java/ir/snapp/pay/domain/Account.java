824cb262e1919f220fe924122da9cb903264fcb0
ali kamali <mohammad.jalalkamali@dfreight.org>
2024-02-10 17:32:37 +0330

    feat: Add fields into Account entity

--- src/main/java/ir/snapp/pay/domain/Account.java
+++ src/main/java/ir/snapp/pay/domain/Account.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -25,6 +26,17 @@ public class Account implements Serializable {
 	@Column(name = "type")
 	@Enumerated(EnumType.STRING)
 	private AccountType type = AccountType.CARD;
+
+	@Size(min = 2, max = 10)
+	@Column(name = "currency", length = 10)
+	private String currency;
+
+	@Size(min = 1, max = 255)
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "balance", scale = 4, precision = 10)
+	private BigDecimal balance;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id", updatable = false, insertable = false)
